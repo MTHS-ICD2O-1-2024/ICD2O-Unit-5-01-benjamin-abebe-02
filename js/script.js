49c81c26c46189b1fa1a6f8797cb10c2c6f54aea
benjamin-abebe <150177783+benjamin-abebe-02@users.noreply.github.com>
2025-04-16 21:26:00 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Benjamin Abebe
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+
+function myButtonClicked() {
+  "use strict";
+
+  // input
+  const userNumber = parseInt(document.getElementById('user-input').value)
+
+
+  // process
+  if (userNumber == randomNumber) {
+    document.getElementById('answer').innerHTML = 
+    '<p>You got the right number!</p>'
+  }
+
+  if (userNumber === randomNumber) {
+    document.getElementById('answer').innerHTML =
+    '<p>You got the right number!</p>'
+  }
+
+  if (userNumber != randomNumber) {
+    document.getElementById('answer').innerHTML =
+      '<p>You got the wrong number!' +
+      '</br>The correct number was:- ' + randomNumber + '.</p>'
+  }
+}
